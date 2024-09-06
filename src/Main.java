05ded54b6ecb15226d39b3976129a9b1b6ce4407
halimAremu <halimat3000@gmail.com>
2024-09-05 19:15:00 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello");
+        System.out.println(calculateCrimeRisk(20));
+        System.out.println(calculateEarthquakeRisk(30));
+        System.out.println(calculateFloodRisk(40));
+        System.out.println(totalCalculatedRisk());
 
+    }
+
+    public static int calculateCrimeRisk(int risk){
+       return risk;
+    }
+    public static int calculateEarthquakeRisk(int risk){
+        return risk;
+    }
+    public static int calculateFloodRisk(int risk){
+       return risk;
+    }
+    public static int totalCalculatedRisk(){
+
+        return ( calculateCrimeRisk(20) + calculateEarthquakeRisk(30) + calculateFloodRisk(40) );
     }
 }
 
